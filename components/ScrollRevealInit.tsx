261681375f6d1325fbac2db2ed588908a45812b2
matinasgarov <matinasgarov21@gmail.com>
2026-06-14 21:31:48 +0400

    feat: add ScrollRevealInit client component

---
+++ components/ScrollRevealInit.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useEffect } from 'react'
+
+export default function ScrollRevealInit() {
+  useEffect(() => {
+    const io = new IntersectionObserver(
+      (entries) =>
+        entries.forEach((e) => {
+          if (e.isIntersecting) e.target.classList.add('in')
+        }),
+      { threshold: 0.08, rootMargin: '0px 0px -40px 0px' }
+    )
+    document.querySelectorAll('.reveal').forEach((el) => io.observe(el))
+    return () => io.disconnect()
+  }, [])
+
+  return null
+}
